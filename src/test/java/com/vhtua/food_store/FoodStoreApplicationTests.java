055bca8bba420413956c180fdedf49d9f2d004e0
vhtua <vhtuananh020402@gmail.com>
2024-11-22 01:26:58 +0700

    init project

---
+++ src/test/java/com/vhtua/food_store/FoodStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vhtua.food_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
